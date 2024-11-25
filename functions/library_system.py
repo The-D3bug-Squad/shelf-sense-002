3ce8ea6b17198d2f7693b746462dd6c0295c1fd2
Nhlakanipho <nhmasiljhb024@student.wethinkcode.co.za>
2024-11-25 13:22:26 +0200

    text file

--- functions/library_system.py
+++ functions/library_system.py
@@ -1,11 +1,16 @@
+import os
+
 DATABASE_FILE = "./database/books.txt"
 
 def initialize_database():
     """
     Initialize the database file if it doesn't exist.
     """
-    with open(DATABASE_FILE, 'a') as db:
-        pass  # Ensure the file exists
+    if not os.path.exists(DATABASE_FILE):
+        with open(DATABASE_FILE, 'a') as db:
+            db.write("")
+
+
 
 def add_book(title, author):
     """
@@ -13,7 +18,12 @@ def add_book(title, author):
     :param title: The title of the book
     :param author: The author of the book
     """
-    # TODO: Append the book's title and author to the database file
+    try:
+        with open(DATABASE_FILE, 'a') as db:
+            db.write(f"{title},{author}")
+    except FileNotFoundError:
+        raise FileNotFoundError(f"{DATABASE_FILE} doesn't exist")
+    
 
 def search_book(title):
     """
@@ -21,11 +31,26 @@ def search_book(title):
     :param title: The title of the book to search for
     :return: A dictionary with the book's details if found, else None
     """
-    # TODO: Implement logic to search for a book in the database file
+    try:
+        with open(DATABASE_FILE, 'r') as db:
+            books = db.readlines()
+            books = {line.strip().split(",")[0]: line.strip().split(",")[1] for line in books if len(line.strip()) > 0}
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Database doesn't exist at {DATABASE_FILE}")
+    
+    return {'title':title, 'author':books[title]} if title in books else None
 
 def list_books():
     """
     List all books in the library.
     :return: A list of dictionaries with each book's details
     """
-    # TODO: Read all books from the database file and return them as a list of dictionaries
+    try:
+        with open(DATABASE_FILE, "r") as db:
+            books = db.readlines()
+            books = [(line.strip().split(",")[0], line.strip().split(",")[1]) for line in books]
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Database doesn't exist at {DATABASE_FILE}")
+    
+    return [{'title': book[0], 'author': book[1]} for book in books if book[0].lower() != 'title']
+
