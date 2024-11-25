eda883730c37092d905944ba153c20df4a9b1b0c
Khens96 <Khensaneo@icloud.com>
2024-11-25 13:06:16 +0200

    shelf

--- functions/library_system.py
+++ functions/library_system.py
@@ -1,4 +1,4 @@
-DATABASE_FILE = "./database/books.txt"
+DATABASE_FILE = "test_books.txt"
 
 def initialize_database():
     """
@@ -15,6 +15,17 @@ def add_book(title, author):
     """
     # TODO: Append the book's title and author to the database file
 
+    add_book("1984", "George Orwell")
+    new_book = {
+        'title': title,
+        'author': author
+    }
+    library.append(new_book)
+    
+    print(f"Book {title}  by {author} has been added to the library.")
+
+
+
 def search_book(title):
     """
     Search for a book by title.
@@ -23,9 +34,46 @@ def search_book(title):
     """
     # TODO: Implement logic to search for a book in the database file
 
+    library = []
+    for book in library:
+        if book [title].lower() == title.lower():
+            return book
+        else:
+            return None
+
+
 def list_books():
     """
     List all books in the library.
     :return: A list of dictionaries with each book's details
     """
     # TODO: Read all books from the database file and return them as a list of dictionaries
+
+    books = []
+    try:
+        with open(DATABASE_FILE, 'r') as file:
+            for line in file:
+                title, author = line.strip().split(", ")
+                book = {
+                    "title": title,
+                    "author": author
+                }
+                books.append(book)
+        
+        return books
+
+    except FileNotFoundError:
+        print("The file 'DATABASE_FILE' was not found")
+        return []
+
+
+
+
+
+
+
+
+
+
+
+
